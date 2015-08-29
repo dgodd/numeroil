f6299350aca93a35cdb27aa3987f389e50bb584c
Dave Goddard <dave@goddard.id.au>
2015-08-29 16:06:04 -0400

    Use boltdb
    Also:
    * allow clear single word (instead of all)

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
-docker run --rm -v "$PWD":/go/src/github.com/dgodd/numeroil -w /go/src/github.com/dgodd/numeroil golang:1.5 go build -o out/numeroil.linux
+#!/usr/bin/env sh
+
+set -e
+
+docker run --rm -v "$PWD":/go/src/github.com/dgodd/numeroil -w /go/src/github.com/dgodd/numeroil golang:1.5 bash -c "go get ./... && go build -o out/numeroil.linux"
+
+ssh wordpress 'mv /home/numeroil/numeroil.linux{,.old}'
+scp out/numeroil.linux wordpress:/home/numeroil/numeroil.linux
+ssh wordpress 'service numeroil restart'
+ssh wordpress 'rm /home/numeroil/numeroil.linux.old'
--- main.go
+++ main.go
@@ -6,8 +6,13 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 
+	"github.com/boltdb/bolt"
 	"github.com/dgodd/numeroil/numeroil"
 )
 
@@ -16,30 +21,96 @@ type Data struct {
 	Big   int
 	Small int
 }
+type Datum []Data
 
-func printData(w io.Writer, data []Data) {
+func (d Datum) Len() int { return len(d) }
+func (d Datum) Less(i, j int) bool {
+	return d[i].Small < d[j].Small || (d[i].Small == d[j].Small && d[i].Big < d[j].Big)
+}
+func (d Datum) Swap(i, j int) {
+	d[i], d[j] = d[j], d[i]
+}
+
+func printData(w io.Writer, data Datum) {
 	fmt.Fprintf(w, "<center><table>")
-	for i := len(data) - 1; i >= 0; i-- {
-		d := data[i]
-		fmt.Fprintf(w, "<tr><td align=center>%s</td><td align=right>%d</td><td align=right>%d</td></tr>", d.Word, d.Big, d.Small)
+	for _, d := range data {
+		fmt.Fprintf(w, "<tr><td align=center>%s</td><td align=right>%d</td><td align=right>%d</td><td><a href='/delete?q=%s' onclick='return confirm(\"Delete? %s\");'>x</a></tr>", d.Word, d.Big, d.Small, url.QueryEscape(d.Word), url.QueryEscape(d.Word))
 	}
 	fmt.Fprintf(w, "</table></center>")
 }
 
-func main() {
-	top := " <html> <head> <link href='https://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'> <style>body{font-family: 'Poiret One', cursive;font-size:30px;background:linen;text-align:center;margin:100px 0;}</style> </head><body> <a href='/clear' style='float:right;'>Clear</a> <form action='/word'> <label for='q'>Word</label> <input name='q' autofocus> <input type='submit'> </form><br><br>"
-	bottom := "</body></html>"
+func exists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
 
-	var data []Data
+func readData(db *bolt.DB) Datum {
+	var data Datum
+	db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("Words"))
+		c := b.Cursor()
+
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			a := strings.Split(string(v), ",")
+			big, e1 := strconv.Atoi(a[0])
+			small, e2 := strconv.Atoi(a[1])
+			if e1 == nil && e2 == nil {
+				data = append(data, Data{Word: string(k), Big: big, Small: small})
+			}
+		}
+
+		return nil
+	})
+	sort.Stable(data)
+	return data
+}
+
+func add(db *bolt.DB, d Data) Datum {
+	db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("Words"))
+		err := b.Put([]byte(d.Word), []byte(fmt.Sprintf("%d,%d", d.Big, d.Small)))
+		return err
+	})
+
+	return readData(db)
+}
+
+func main() {
+	db, err := bolt.Open("my.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.Update(func(tx *bolt.Tx) error {
+		tx.CreateBucket([]byte("Words"))
+		tx.CreateBucket([]byte("ClearedWords"))
+		return nil
+	})
+	defer db.Close()
+
+	top := " <html> <head> <link href='https://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'> <style>body{font-family: 'Poiret One', cursive;font-size:30px;background:linen;text-align:center;margin:100px 0;}</style> </head><body> <form action='/word'> <label for='q'>Word</label> <input name='q' autofocus> <input type='submit'> </form><br><br>"
+	bottom := "</body></html>"
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, top)
+		data := readData(db)
 		printData(w, data)
 		fmt.Fprintf(w, bottom)
 	})
 
-	http.HandleFunc("/clear", func(w http.ResponseWriter, r *http.Request) {
-		data = data[:0]
+	http.HandleFunc("/delete", func(w http.ResponseWriter, r *http.Request) {
+		word := []byte(r.FormValue("q"))
+		db.Update(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte("Words"))
+			dead := tx.Bucket([]byte("ClearedWords"))
+			data := b.Get(word)
+			b.Delete(word)
+			err := dead.Put(word, data)
+			return err
+		})
 		http.Redirect(w, r, "/", http.StatusFound)
 	})
 
@@ -47,10 +118,10 @@ func main() {
 		word := r.FormValue("q")
 		big := numeroil.AddLetters(word)
 		small := numeroil.Reduce(big)
-		data = append(data, Data{Word: word, Big: big, Small: small})
+		data := add(db, Data{Word: word, Big: big, Small: small})
 
 		fmt.Fprintf(w, top)
-		fmt.Fprintf(w, "%q <br> Big: %d <br> small: %d", html.EscapeString(word), big, small)
+		fmt.Fprintf(w, "%q <br> Big: %d <br> small: %d <br><br>", html.EscapeString(word), big, small)
 		printData(w, data)
 		fmt.Fprintf(w, bottom)
 	})
